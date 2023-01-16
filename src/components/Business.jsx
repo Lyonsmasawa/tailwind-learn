7060db2f5a452a05e4cbe4220a8807abaf0795c5
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-01-16 10:33:16 -0800

    create: business page

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import styles, {layout} from '../style'
+import { features } from '../constants'
+import Button from './Button'
+
+const FeatureCard = () => (
+  <div className=""></div>
+)
 
 const Business = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>You do the business, <br className='sm:block hidden' /> we'll handle the money</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right  credit card, you can improve your financial life by building credit, earning rewards and saving money. but with hundreds of credit cards on the market</p>
+     <Button styles='mt-10' />
+     </div>
+
+     <div className={`${layout.sectionImg} flex-col-reverse`}>
+        {features.map((feature) => (
+          <FeatureCard />
+        ))}
+     </div>
+    </section>
   )
 }
 
